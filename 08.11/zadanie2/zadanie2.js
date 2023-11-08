a8cc778d6ebaf4f1be5c0af4063691960f79a166
Dankino <89976283+Kinowski-Daniel@users.noreply.github.com>
2023-11-08 17:13:41 +0100

    Add files via upload

---
+++ 08.11/zadanie2/zadanie2.js
@@ -0,0 +1,6 @@
+function Fibonacci(n) {
+    if(n < 2) return n;
+    return (Fibonacci(n - 1) + Fibonacci(n - 2));
+}
+
+for(i = 0; i < 30; i++) console.log(Fibonacci(i));
---
+++ 08.11/zadanie3/zadanie3.js
@@ -0,0 +1,7 @@
+let tablica1 = [1, 2, 3];
+let tablica2 = [4, 5, 6];
+let połączoneTabele = tablica1.concat(tablica2);//sposób 1
+
+const mergeResult = [tablica1, tablica2];//sposób 2
+
+mergeResult;

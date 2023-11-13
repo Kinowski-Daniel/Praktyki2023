ee0c314ca08079aa0eec1132ccc10a5c55584ed7
Dankino <89976283+Kinowski-Daniel@users.noreply.github.com>
2023-11-13 16:51:02 +0100

    Update palindrome.js
    Zmieniona funkcja palindrome

--- Zadania 07.11/zadanie 2/palindrome.js
+++ Zadania 07.11/zadanie 2/palindrome.js
@@ -1,4 +1,5 @@
-function sprawdz() {
+//Poprzednia wersja
+/*function sprawdz() {
     const text = document.getElementById('text').value;
     const resultElement = document.getElementById('wynik');
 
@@ -13,4 +14,31 @@ function palindrome(str) {
     str = str.replace(/ /g, "").toLowerCase();
     const reversedStr = str.split("").reverse().join("");
     return str === reversedStr;
+}*/
+
+//Nowa wersja
+function sprawdz() {
+    const text = document.getElementById('text').value;
+    const resultElement = document.getElementById('wynik');
+
+    if (palindrome(text)) {
+        resultElement.textContent = "To jest palindrom";
+    } else {
+        resultElement.textContent = "To nie jest palindrom";
+    }
 }
+
+function palindrome(str) {
+    str = str.replace(/ /g, "").toLowerCase();
+    const len = str.length;
+
+    for (let i = 0; i < Math.floor(len / 2); i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
